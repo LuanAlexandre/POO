8fc3a7ecea8b7404d14e27d6157c3cbc5da32faa
Luan Alexandre <luanrb19@gmail.com>
2024-05-07 18:05:13 -0400

    Laboratorio 2 - Questao 2

---
+++ Lab-RelembrandoProgramacao/src/Macaco.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author luan-dev
+ */
+public class Macaco {
+    public static void main(String[] args) {
+        System.out.println("  *****  ");
+        System.out.println("(* o o *)");
+        System.out.println(" *  ^  * ");
+        System.out.println(" * --- * ");
+        System.out.println("  *****  ");
+    }
+}
