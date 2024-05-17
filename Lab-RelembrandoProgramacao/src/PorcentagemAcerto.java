f663c21b187cc3cc2a74aee84df47b5def84fae7
Luan Alexandre <luan.barreto@icomp.ufam.edu.br>
2024-05-16 22:56:23 -0400

    Laboratorio 2 - Questao 29

---
+++ Lab-RelembrandoProgramacao/src/PorcentagemAcerto.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author axlre
+ */
+import java.util.Scanner;
+
+public class PorcentagemAcerto {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int[] answers = new int[100];
+        int[] studentAnswers = new int[50];
+        int[] trueAnswers = new int[50];
+        int answer;
+        int countFlag = 0; //conta a quantidade de -1 digitados
+        int indexStudent = 0;
+        int indexTrue = 0;
+        double hits = 0;
+        
+        while (countFlag < 2) {
+            answer = scan.nextInt();
+            if(countFlag == 0) {
+                studentAnswers[indexStudent] = answer;
+                indexStudent++;
+            }
+            if(countFlag == 1) {
+                trueAnswers[indexTrue] = answer;
+                indexTrue++;
+            }
+            if (answer == -1) countFlag++;
+        }        
+                
+        for (int i=0; i<indexStudent-1; i++) {
+            if(studentAnswers[i] == trueAnswers[i]) {
+                hits++;
+            }
+        }
+                
+        System.out.printf("%.2f\n", (hits/(indexStudent-1))*100);
+        
+        scan.close();
+    }
+}
