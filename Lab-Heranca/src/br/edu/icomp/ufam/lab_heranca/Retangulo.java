a809390f4e0a8f5986f931411024d4ce8aa91463
Luan Alexandre <luanrb19@gmail.com>
2024-05-21 10:46:23 -0400

    Lab 6 - Questao 3: Classe Retangulo

---
+++ Lab-Heranca/src/br/edu/icomp/ufam/lab_heranca/Retangulo.java
@@ -0,0 +1,34 @@
+package br.edu.icomp.ufam.lab_heranca;
+
+public class Retangulo extends FormaGeometrica{
+    public double altura;
+    public double largura;
+
+    public Retangulo(int posX, int posY, double largura, double altura) {
+        super(posX, posY);
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+    public double getArea() {
+        double area = this.altura * this.largura;
+        
+        return area;
+    }
+    
+    public double getPerimetro() {
+        double perimetro = 2*(this.altura + this.largura);
+
+        return perimetro;
+    }
+
+    public String toString() {
+        String posicao = getPosString();
+        double area = getArea();
+        double perimetro = getPerimetro();
+        String description = String.format("Retângulo na %s com largura de %.1fcm e altura de %.1fcm (área=%.1fcm2, perímetro=%.1fcm)",
+        posicao, largura, altura, area, perimetro);
+
+        return description;
+    }
+}
